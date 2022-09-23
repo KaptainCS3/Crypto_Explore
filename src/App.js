ee558f5b01af4188ddd6a24b1f66c546e7ee6062
KaptainCS3 <mbiapplegryn@gmail.com>
2022-09-23 20:54:51 +0100

    setup files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
+import { style } from "@mui/system";
 import React from "react";
-const App = () =>{
-    return (
-    );
-}
+import styled from "styled-components";
+const App = () => {
+  return(
+    <div className="container-app">
+      <button>Edit</button>
+      <h1 className="heading">Folders</h1>
+      <input type='search' />
+      <h3>Storage</h3>
+      <Notes>
+
+      </Notes>
+    </div>
+  );
+};
 export default App;
+
+const Notes = styled.div`
+
+`

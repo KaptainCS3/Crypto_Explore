fc914e9dcdd93a94e1cd5e7475a52481a455e8c2
KaptainCS3 <mbiapplegryn@gmail.com>
2022-09-26 22:14:52 +0100

    style component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,66 @@
 import { style } from "@mui/system";
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import { CreateNewFolder, Search } from "@mui/icons-material";
 const App = () => {
-  return(
-    <div className="container-app">
-      <button>Edit</button>
+  const [searchData, setSearchData] = useState({
+    search: "",
+  });
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setSearchData((prevData) => {
+      return {
+        ...prevData,
+        [name]: value,
+      };
+    });
+  }
+  return (
+    <div className="container">
+      <button className="edit">Edit</button>
       <h1 className="heading">Folders</h1>
-      <input type='search' />
-      <h3>Storage</h3>
+      <Wrapper></Wrapper>
+      <Search className="search-icon"></Search>
+      <input
+        className="search-bar"
+        type="search"
+        name="search"
+        // value={searchData}
+        onChange={handleChange}
+      />
+      <h3 className="file--location">Storage</h3>
       <Notes>
-
+        <NoteContainer>d</NoteContainer>
       </Notes>
+      <Directory>
+        <CreateNewFolder></CreateNewFolder>
+        <AddNote></AddNote>
+      </Directory>
     </div>
   );
 };
 export default App;
 
 const Notes = styled.div`
-
-`
+  width: 100%;
+  height: 100%;
+`;
+const NoteContainer = styled.div`
+  background: #fff;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  padding: 0 5px;
+  border-radius: 10px;
+  `;
+  const Directory = styled.div`
+  position: fixed;
+  bottom: 5px;
+  background: #999;
+  height: 35px;
+  width: 100%;
+  margin: 0 5px;
+  over-flow: hidden;
+`;
+const AddNote = styled.div``;
+const Wrapper = styled.div``;

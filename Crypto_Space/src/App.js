77bf10227ec0bb956246f7224337bfdde42a8275
KaptainCS3 <mbiapplegryn@gmail.com>
2023-04-07 23:22:29 +0100

    update App

--- Crypto_Space/src/App.js
+++ Crypto_Space/src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
 import "./App.css";
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
 const App = () => {
-  return <div>Hello There</div>;
+  return (
+    <div className="bg-[#F2F8FE]">
+      <Nav />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;

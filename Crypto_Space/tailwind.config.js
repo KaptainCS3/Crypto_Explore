3fc0123aec4e139530f134543daccc4a176ce830
KaptainCS3 <mbiapplegryn@gmail.com>
2023-04-08 23:30:45 +0100

    rename media screen seizes

--- Crypto_Space/tailwind.config.js
+++ Crypto_Space/tailwind.config.js
@@ -9,14 +9,14 @@ module.exports = {
       // },
     },
     screens: {
-      sm: { min: "200px", max: "475px" },
-      // => @media (min-width: 200px and max-width: 475px) { ... }
+      sm: { min: "200px", max: "768px" },
+      // => @media (min-width: 200px and max-width: 768px) { ... }
 
-      md: { min: "476px", max: "768px" },
-      // => @media (min-width: 476px and max-width: 768px) { ... }
+      md: { min: "769px", max: "990px" },
+      // => @media (min-width: 769px and max-width: 990px) { ... }
 
-      lg: { min: "769px" },
-      // => @media (min-width: 769px ) { ... }
+      lg: { min: "991px" },
+      // => @media (min-width: 991px ) { ... }
     },
   },
   plugins: [],

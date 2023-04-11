0fc649aad0006d176ca4d4cc1a52d20b86d170fe
KaptainCS3 <mbiapplegryn@gmail.com>
2023-04-11 22:37:28 +0100

    include nav, footer, about component

---
+++ Crypto_Explore/src/pages/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import About from '../components/About'
+import Nav from '../components/Nav'
+import Footer from '../components/Footer'
+const About = () => {
+  return (
+    <div>
+        <Nav />
+        <Footer />
+    </div>
+  )
+}
+
+export default About

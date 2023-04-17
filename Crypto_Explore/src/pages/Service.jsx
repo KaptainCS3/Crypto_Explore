cda3dd36ad63df6c1237e3973a19c1c0c47c674a
KaptainCS3 <mbiapplegryn@gmail.com>
2023-04-17 23:22:18 +0100

    initial commit

---
+++ Crypto_Explore/src/pages/Service.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Nav from '../components/Nav'
+import Services from '../components/Services'
+import CurrentTab from '../components/CurrentTab'
+import Footer from '../components/Footer'
+
+const Service = () => {
+  return (
+    <div>
+        <Nav />
+        <CurrentTab />
+        <Services />
+        <Footer />
+    </div>
+  )
+}
+
+export default Service

827306a4363290d3a2a4eead34f2292238d9c756
KaptainCS3 <mbiapplegryn@gmail.com>
2023-04-27 15:27:12 +0100

    initial commit

---
+++ Crypto_Explore/faq.js
@@ -0,0 +1,42 @@
+export default [
+  {
+    question: "How to build a website?",
+    result:
+      "Dolor nonumy tempor elitr et rebum ipsum sit duo duo. Diam sed sed magna et magna diam aliquyam amet dolore ipsum erat duo. Sit rebum magna duo labore no diam.",
+  },
+  {
+    question: "How long will it take to get a new website?",
+    result:
+      "Dolor nonumy tempor elitr et rebum ipsum sit duo duo. Diam sed sed magna et magna diam aliquyam amet dolore ipsum erat duo. Sit rebum magna duo labore no diam.",
+  },
+  {
+    question: "Do you create HTML websites?",
+    result:
+      "Dolor nonumy tempor elitr et rebum ipsum sit duo duo. Diam sed sed magna et magna diam aliquyam amet dolore ipsum erat duo. Sit rebum magna duo labore no diam.",
+  },
+  {
+    question: "Will my website be mobile-friendly?",
+    result:
+      "Dolor nonumy tempor elitr et rebum ipsum sit duo duo. Diam sed sed magna et magna diam aliquyam amet dolore ipsum erat duo. Sit rebum magna duo labore no diam.",
+  },
+  {
+    question: "Will you maintain my website for me?",
+    result:
+      "Dolor nonumy tempor elitr et rebum ipsum sit duo duo. Diam sed sed magna et magna diam aliquyam amet dolore ipsum erat duo. Sit rebum magna duo labore no diam.",
+  },
+  {
+    question: "I'm on a strict budget. Do you have any low cost options?",
+    result:
+      "Dolor nonumy tempor elitr et rebum ipsum sit duo duo. Diam sed sed magna et magna diam aliquyam amet dolore ipsum erat duo. Sit rebum magna duo labore no diam.",
+  },
+  {
+    question: "I'm on a strict budget. Do you have any low cost options?",
+    result:
+      "Dolor nonumy tempor elitr et rebum ipsum sit duo duo. Diam sed sed magna et magna diam aliquyam amet dolore ipsum erat duo. Sit rebum magna duo labore no diam.",
+  },
+  {
+    question: "I'm on a strict budget. Do you have any low cost options?",
+    result:
+      "Dolor nonumy tempor elitr et rebum ipsum sit duo duo. Diam sed sed magna et magna diam aliquyam amet dolore ipsum erat duo. Sit rebum magna duo labore no diam.",
+  },
+];

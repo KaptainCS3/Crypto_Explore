dd75efc8159f1f8082ee7c5afc1cfd2b8ebc3ed7
KaptainCS3 <mbiapplegryn@gmail.com>
2023-04-29 12:16:49 +0100

    current tab done

--- Crypto_Explore/src/components/CurrentTab.jsx
+++ Crypto_Explore/src/components/CurrentTab.jsx
@@ -1,37 +1,36 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const CurrentTab = () => {
+const CurrentTab = (props) => {
   return (
     <>
-    <section className="bg-[#F2F8FE] mb-12 py-[6rem] h-full sm:w-full w-max[540px] mx-auto flex flex-col justify-between px-4 font-['Open Sans'] md:w-full mx-auto">
-        <div className='w-[85%] mx-auto lg:flex justify-between'>
-
-        <div className='mb-12 lg:w-1/2'>
-            <h1 className='hero font-[700] mb-4'>About Us</h1>
+      <section className="bg-[#F2F8FE] mb-12 py-[6rem] h-full sm:w-full w-max[540px] mx-auto flex flex-col justify-between font-['Open Sans'] md:w-full mx-auto">
+        <div className="w-[85%] mx-auto lg:flex justify-between">
+          <div className="mb-12 lg:w-1/2">
+            <h1 className="hero font-[700] mb-4">{props.path}</h1>
             <nav>
-                <ol className='flex'>
-                    <li className="text-[#16D5FF] hover:text-[#0d6efd] cursor-pointer after:content-['/'] after:px-2">
-                        <Link to="">
-                        Home
-                        </Link>
-                        </li>
-                    <li className="text-[#16D5FF] hover:text-[#0d6efd]  cursor-pointer after:content-['/'] after:px-2">
-                        <Link to="">
-                        Pages
-                        </Link>
-                        </li>
-                    <li className='text-[#999]'>About Us</li>
-                </ol>
+              <ol className="flex">
+                <li className="text-[#16D5FF] hover:text-[#0d6efd] cursor-pointer after:content-['/'] after:text-[#999] after:px-2">
+                  <Link to="">Home</Link>
+                </li>
+                <li className="text-[#16D5FF] hover:text-[#0d6efd]  cursor-pointer after:content-['/'] after:text-[#999] after:px-2">
+                  <Link to="">Pages</Link>
+                </li>
+                <li className="text-[#999]">{props.path}</li>
+              </ol>
             </nav>
+          </div>
+          <div className="animate-pulse flex items-center justify-center lg:w-[48%]">
+            <img
+              src={"/assets/hero-2.png"}
+              alt="hero-2"
+              className="max-w-full h-auto"
+            />
+          </div>
         </div>
-        <div className='animate-pulse flex items-center justify-center lg:w-[48%]'>
-            <img src={("/assets/hero-2.png")} alt="hero-2" className='max-w-full h-auto'/>
-        </div>
-        </div>
-    </section>
+      </section>
     </>
-  )
+  );
 }
 
 export default CurrentTab

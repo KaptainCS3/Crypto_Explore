621b2743b677d886140ac6b00e51c71107999011
KaptainCS3 <mbiapplegryn@gmail.com>
2023-04-29 21:47:28 +0100

    transaction component done

--- Crypto_Explore/src/components/Transaction.jsx
+++ Crypto_Explore/src/components/Transaction.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
-
+import CountUp, { useCountUp } from 'react-countup';
 const Statistic = () => {
+  useCountUp({
+    ref: 'counter',
+    end: 123456,
+    enableScrollSpy: true,
+    scrollSpyDelay: 1000,
+  });
   return (
      <>
          <section className="mb-12 py-12 h-full sm:w-full w-max[540px] mx-auto flex flex-col justify-between px-4 font-['Open Sans'] md:w-full mx-auto">
@@ -11,21 +17,27 @@ const Statistic = () => {
               <div>
               <img src="/assets/icon-9.png" alt="icon-9" className='h-auto' />
               </div>
-              <h3 className='hero font-[700] mt-4'>123456</h3>
+              <h3 className='hero font-[700] mt-4'>
+                <CountUp end={123456} separator='' enableScrollSpy={true} ScrollSpyOnce={true} />
+              </h3>
               <p className='text-[#16D5FF] text-xl'>Today Transactions</p>
             </div>
             <div className='mt-8 w-full flex flex-col items-center'>
               <div>
               <img src="/assets/icon-10.png" alt="icon-10" className='h-auto' />
               </div>
-              <h3 className='hero font-[700] mt-4'>123456</h3>
+              <h3 className='hero font-[700] mt-4'>
+                <CountUp end={123456} separator='' enableScrollSpy={true} ScrollSpyOnce={true} />
+              </h3>
               <p className='text-[#16D5FF] text-xl'>Monthly Transactions</p>
             </div>
             <div className='mt-8 w-full flex flex-col items-center'>
               <div>
               <img src="/assets/icon-2.png" alt="icon-2" className='h-auto' />
               </div>
-              <h3 className='hero font-[700] mt-4'>123456</h3>
+              <h3 className='hero font-[700] mt-4'>
+                <CountUp end={123456} separator='' enableScrollSpy={true} ScrollSpyOnce={true} />
+              </h3>
               <p className='text-[#16D5FF] text-xl'>Total Transactions</p>
             </div>
           </div>

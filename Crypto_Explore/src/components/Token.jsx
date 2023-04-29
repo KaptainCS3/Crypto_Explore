b07bf50b4fad8c60fef3d7ae89e615fff090e3da
KaptainCS3 <mbiapplegryn@gmail.com>
2023-04-29 11:37:51 +0100

    token component done

--- Crypto_Explore/src/components/Token.jsx
+++ Crypto_Explore/src/components/Token.jsx
@@ -1,40 +1,50 @@
 import React from 'react'
-
-const Token = () => {
+import Fade from 'react-reveal/Fade';
+const Token = (props) => {
   return (
     <>
-      <section className="mb-12 py-12 h-full sm:w-full w-max[540px] mx-auto flex flex-col justify-between px-4 font-['Open Sans'] md:w-full mx-auto">
+      <section className="mb-12 py-12 h-full sm:w-full w-max[540px] mx-auto flex flex-col justify-between font-['Open Sans'] md:w-full mx-auto">
         {/* Token Sales section */}
         <div className="w-[85%] mx-auto">
           <div className='flex flex-col items-center justify-center'>
-            <h1 className='font-size mb-2 font-bold'>Token Sale</h1>
-            <p className='text-[rgb(22,213,255)] text-xl mb-6'>Token Sale Countdown</p>
+            <Fade bottom>
+                <h1 className='font-size mb-2 font-bold'>Token Sale</h1>
+                <p className='text-[rgb(22,213,255)] text-xl mb-6'>Token Sale Countdown</p>
+            </Fade>
             <div className='w-full'>
             <div className='sm:flex md:flex flex-row justify-between mb-4 lg:flex'>
               <div className='sm:w-full mr-2 md:flex w-full lg:flex'>
-            <div className='mt-4 w-full flex flex-col justify-center items-center bg-white p-4 md:'>
-              <h2 className='text-4xl font-bold'>0</h2>
-              <p className='text-[#16D5FF] text-2xl'>Days</p>
-            </div>
-            <div className='mt-4 w-full flex flex-col justify-center items-center bg-white p-4 lg:ml-8 mr-4'>
-               <h2 className='text-4xl font-bold'>0</h2>
-               <p className='text-[#16D5FF] text-2xl'>Hours</p>
-            </div>
+              <Fade left>
+                <div className={`mt-4 w-full flex flex-col justify-center items-center ${props.background} p-4`}>
+                  <h2 className='text-4xl font-bold'>0</h2>
+                  <p className='text-[#16D5FF] text-2xl'>Days</p>
+                </div>
+              </Fade>
+              <Fade left>
+                <div className={`mt-4 w-full flex flex-col justify-center items-center ${props.background} p-4 md:ml-4 lg:ml-8 lg:mr-4`}>
+                  <h2 className='text-4xl font-bold'>0</h2>
+                  <p className='text-[#16D5FF] text-2xl'>Hours</p>
+                </div>
+              </Fade>
             </div>
             <div className='sm:w-full ml-2 md:flex w-full lg:flex'>
-            <div className='mt-4 w-full flex flex-col justify-center items-center bg-white p-4 mr-8'>
-                <h2 className='text-4xl font-bold'>0</h2>
-                <p className='text-[#16D5FF] text-2xl'>Minutes</p>
-            </div>
-            <div className='mt-4 w-full flex flex-col justify-center items-center bg-white p-4'>
+            <Fade left>
+              <div className={`mt-4 w-full flex flex-col justify-center items-center ${props.background} p-4 md:mr-4 lg:mr-8`}>
+                  <h2 className='text-4xl font-bold'>0</h2>
+                  <p className='text-[#16D5FF] text-2xl'>Minutes</p>
+              </div>
+            </Fade>
+            <Fade left>
+            <div className={`mt-4 w-full flex flex-col justify-center items-center ${props.background} p-4`}>
                 <h2 className='text-4xl font-bold'>0</h2>
                 <p className='text-[#16D5FF] text-2xl'>Seconds</p>
             </div>
+            </Fade>
             </div>
            </div>
           </div>
           <button className='my-8 px-8 py-4 bg-[#16D5FF] rounded-md text-white text-xl'>Buy Token</button>
-          <div className='flex mx-4 mb-12 sm:w-1/2 md:w-3/4'>
+          <div className='flex mx-4 mb-12 sm:w-1/2 md:w-3/4 lg:w-1/5'>
             <div className='w-full h-auto'>
               <img src="/assets/payment-1.png" alt="payment-1" className='sm:w-full' />
             </div>

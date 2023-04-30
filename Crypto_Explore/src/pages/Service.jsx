eaa250c6c4a7b5149a0ab4f35cebd3186e710bd6
KaptainCS3 <mbiapplegryn@gmail.com>
2023-04-30 22:11:54 +0100

    fixed props passing

--- Crypto_Explore/src/pages/Service.jsx
+++ Crypto_Explore/src/pages/Service.jsx
@@ -1,18 +1,25 @@
-import React from 'react'
-import Nav from '../components/Nav'
-import Services from '../components/Services'
-import CurrentTab from '../components/CurrentTab'
-import Footer from '../components/Footer'
+import React from "react";
+import Nav from "../components/Nav";
+import Services from "../components/Services";
+import CurrentTab from "../components/CurrentTab";
+import Footer from "../components/Footer";
+import ScrollTop from "../components/ScrollTop";
 
 const Service = () => {
   return (
     <div>
-        <Nav />
-        <CurrentTab />
-        <Services />
-        <Footer />
+      <Nav />
+      <CurrentTab path="Services" />
+      <Services
+        background="bg-[#F2F8FE]"
+        color="after:border-r-white"
+        borderRight="hover:after:border-r-[#F2F8FE]"
+        mainBackground="bg-white"
+      />
+      <ScrollTop />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Service
+export default Service;

f99e96ff91242dc18d28367035c476a3e73e731e
KaptainCS3 <mbiapplegryn@gmail.com>
2023-04-30 22:13:24 +0100

    no form validation included

--- Crypto_Explore/src/pages/Contact.jsx
+++ Crypto_Explore/src/pages/Contact.jsx
@@ -1,19 +1,21 @@
-import React from 'react'
-import Nav from '../components/Nav'
-import CurrentTab from '../components/CurrentTab'
-import ContactForm from '../components/ContactForm'
-import Footer from '../components/Footer'
-import ScrollTop from '../components/ScrollTop'
+import React from "react";
+import Nav from "../components/Nav";
+import CurrentTab from "../components/CurrentTab";
+import ContactForm from "../components/ContactForm";
+import Footer from "../components/Footer";
+import ScrollTop from "../components/ScrollTop";
+import MapView from "../components/MapView";
 const Contact = () => {
   return (
     <div>
-        <Nav />
-        <CurrentTab path="Contact Us" />
-        <ContactForm />
-        <ScrollTop />
-        <Footer />
+      <Nav />
+      <CurrentTab path="Contact Us" />
+      <ContactForm />
+      <MapView />
+      <ScrollTop />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
